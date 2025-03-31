a22848b52025fa44f3d732a29aa62a7fa2fb79d0
SeigiMondragon <sonic92yeylol@gmail.com>
2025-03-31 20:39:45 +0800

    JsonArrayRequest Values Extracted (Unchecked & Untested)

--- app/src/main/java/com/example/madmidterm/MainActivity.java
+++ app/src/main/java/com/example/madmidterm/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.madmidterm;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +23,36 @@ public class MainActivity extends AppCompatActivity {
 
         String url = "https://mocki.io/v1/2e6af150-20a1-4966-a6c7-100331830790";
 
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, url,null,
 
-        JSON
+                response->{
+                    try{
+                        String [] characters = new String[response.length()];
+
+                        JSONObject bookObject = response.getJSONObject(0);
+                        String bookName = bookObject.getString("BookName");
+                        int bookChapter = bookObject.getInt("Chapters");
+                        int bookVerses = bookObject.getInt("Verses");
+                        JSONArray charactersJsonArray = bookObject.getJSONArray("Characters");
+                        for (int i = 0; i< charactersJsonArray.length(); i++){
+                            characters [i]  = charactersJsonArray.getString(i);
+                        }
+
+                        JSONObject typeObject = bookObject.getJSONObject("Type");
+                        String deliveryType = typeObject.getString("Delivery Type");
+                        String literatureType = typeObject.getString("Literature Type");
+                        String testamentType = typeObject.getString("Testament Type");
+
+
+
+                    }catch (Exception e){
+                        Toast.makeText(this, "Error has occurred", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                error -> {
+
+                }
+        );
     }
+
 }
